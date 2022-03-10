217e1b02697f9e4ad7bb1666bbeced70aefd61cf
Zaur Kurbanismailov <kurbanismailov.92@gmail.com>
2022-03-10 19:44:36 +0300

    Added test scripts

---
+++ Assets/Scripts/SomeClass.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace EditorScriptsTemplates
+{
+	public class SomeClass
+	{
+	}
+}
